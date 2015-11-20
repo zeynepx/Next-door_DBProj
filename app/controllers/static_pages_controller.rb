7412b2e27dda6a2d78008325774a5e2e849b23e8
Xiaolong Jiang <xljiangtju2010@gmail.com>
2015-11-20 10:43:07 -0500

    Finish home, help, about static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
 class StaticPagesController < ApplicationController
+  # This is a home action created in controller
   def home
   end
 
   def help
+  end
+
+  def about
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Nextdoor"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Nextdoor"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Nextdoor"
   end
 
 end
