dbebb9df36013d529192aed0aef54d04024d29df
Xiaolong Jiang <xljiangtju2010@gmail.com>
2015-11-25 23:31:46 -0500

    Add profile attribute to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126031102) do
+ActiveRecord::Schema.define(version: 20151126041441) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",            limit: 255
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151126031102) do
     t.datetime "updated_at",                                  null: false
     t.string   "password_digest", limit: 255
     t.boolean  "admin",                       default: false
+    t.string   "profile",         limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
